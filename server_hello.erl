78d508ccd633ce6a0bfee3698968cd20c45631f5
Chris Smith <csmith@scottlogic.co.uk>
2016-01-15 16:19:20 +0000

    Route to back end node based on path portion of URL

--- server_hello.erl
+++ server_hello.erl
@@ -21,6 +21,15 @@ plain_text_response(Text) ->
     Length = integer_to_list(string:len(Text)),
     "HTTP/1.1 200 OK\r\nContent-Length: " ++ Length ++ "\r\nContent-Type: text/plain\r\n\r\n" ++ Text.
 
+%% Paths beginning in the first half of the alphabet go to the first back end node
+%% The second half go to the second back end node
 partition_for(Bin) ->
-    %% TODO: Implement based on path for GET request
-    blah.
+    [Line|_] = string:tokens(binary_to_list(Bin), "\r\n"), %% First line is the GET request
+    Code = lists:nth(6, Line), %% "GET /" are the first 5 characters
+    if
+        Code > 64, Code < 78 -> first; %% A - M
+	Code > 96, Code < 110 -> first; %% a - m
+	Code > 77, Code < 91 -> second; %% N - Z
+	Code > 109, Code < 123 -> second; %% n - z
+	true -> error
+    end.
