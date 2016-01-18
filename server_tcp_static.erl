23b5890148beeb64dd341055ed688c7de40f0884
Chris Smith <csmith@scottlogic.co.uk>
2016-01-18 14:36:27 +0000

    Make static TCP server persistent, i.e. no longer one-shot

--- server_tcp_static.erl
+++ server_tcp_static.erl
@@ -3,15 +3,18 @@
 
 start(Response) ->
     {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 0}, {reuseaddr, true}, {active, true}]),
-    {ok, Socket} = gen_tcp:accept(Listen),
-    gen_tcp:close(Listen),
-    loop(Socket, Response).
+    accept(Listen, Response).
 
-loop(Socket, Response) ->
+accept(Listen, Response) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    respond(Socket, Response),
+    accept(Listen, Response).
+
+respond(Socket, Response) ->
     receive
         {tcp, Socket, _Bin} ->
             gen_tcp:send(Socket, Response),
-            loop(Socket, Response);
+            respond(Socket, Response);
         {tcp_closed, Socket} ->
-            io:format("Server socket closed~n")
+            io:format("The client closed the connection~n")
     end.
